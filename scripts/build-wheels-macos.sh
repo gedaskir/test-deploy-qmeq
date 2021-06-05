0f9264f8a695b62eedab54b435aa5177c917f7a1
Gediminas Kirsanskas <gediminas.kirsanskas@gmail.com>
2021-06-05 21:19:55 +0200

    Try to list bin folder.

--- scripts/build-wheels-macos.sh
+++ scripts/build-wheels-macos.sh
@@ -4,11 +4,14 @@ set -e -x
 # Choose the directory containing the latest version of GCC
 # as indicated by the highest number suffixed to
 # the filepath of the package directory
-print -v version /usr/local/opt/gcc@<->(n[-1])
-version=${version#*@}
-for file in /usr/local/opt/gcc@${version}/bin/*-${version}(*); do
-    tail=${file:t}
-    ln -sf ${file} /usr/local/bin/${tail%-*}
-done
+
+echo "opt"
+ls /usr/local/opt/
+
+echo "local/bin"
+ls /usr/local/bin/
+
+echo "bin"
+ls /usr/bin/
 
 gcc --version
