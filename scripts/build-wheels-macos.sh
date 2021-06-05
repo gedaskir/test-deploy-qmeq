3e8339140ff0e0ebd984b2c89931cf5b0f5aa01d
Gediminas Kirsanskas <gediminas.kirsanskas@gmail.com>
2021-06-05 21:12:43 +0200

    Add a script to create symbolic link to gcc on MacOS.

--- scripts/build-wheels-macos.sh
+++ scripts/build-wheels-macos.sh
@@ -1,42 +1,14 @@
 #!/bin/bash
 set -e -x
 
-# Different Python versions separated by space
-VERS=("3.6" "3.7" "3.8" "3.9")
-len=${#VERS[@]}
-
-# Links to different Python versions for MacOS
-URLS=("https://www.python.org/ftp/python/3.6.8/python-3.6.8-macosx10.9.pkg",
-      "https://www.python.org/ftp/python/3.7.9/python-3.7.9-macosx10.9.pkg",
-      "https://www.python.org/ftp/python/3.8.10/python-3.8.10-macosx10.9.pkg",
-      "https://www.python.org/ftp/python/3.9.5/python-3.9.5-macosx10.9.pkg")
-
-PYPATH="/Library/Frameworks/Python.framework/Versions/"
-
-for (( i=0; i<${len}; i++ )); do
-    echo ${VERS[$i]}
-    echo ${URLS[$i]}
-    PNAME="/tmp/Python-${VERS[$i]}.pkg"
-    curl -L -o $PNAME ${URLS[$i]}
-    sudo installer -pkg $PNAME -target /
-    #
-    PYBIN="${PYPATH}/${VERS[$i]}/bin"
-    sudo ln -sf "${PYBIN}/python${VERS[$i]}" "${PYBIN}/python"
-    sudo ln -sf "${PYBIN}/pip${VERS[$i]}" "${PYBIN}/pip"
+# Choose the directory containing the latest version of GCC
+# as indicated by the highest number suffixed to
+# the filepath of the package directory
+print -v version /usr/local/opt/gcc@<->(n[-1])
+version=${version#*@}
+for file in /usr/local/opt/gcc@${version}/bin/*-${version}(*); do
+    tail=${file:t}
+    ln -sf ${file} /usr/local/bin/${tail%-*}
 done
 
-# Compile wheels and put them into mywheels/
-for PYBIN in ${VERS[@]}; do
-    PYBIN="${PYPATH}/${PYBIN}/bin"
-    "${PYBIN}/pip" install -r scripts/requirements.txt
-    "${PYBIN}/python" setup.py bdist_wheel -d mywheels
-    "${PYBIN}/python" clean.py
-done
-
-# Install packages and test
-mv mywheels dist
-for PYBIN in ${VERS[@]}; do
-    PYBIN="${PYPATH}/${PYBIN}/bin"
-    "${PYBIN}/pip" install qmeq --no-index -f dist
-    "${PYBIN}/pytest" --pyargs qmeq
-done
+gcc --version
